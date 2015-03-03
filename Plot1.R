c01e9bd6cd57e019fe05b81667e6844a0f84c560
peter111 <1peter.horvat@gmail.com>
2015-03-03 01:31:28 +0100

    Project1
    Source files and plots for project1

--- Plot1.R
+++ Plot1.R
@@ -1,6 +1,7 @@
 ## loading dataset 
 setwd("C://Users//Peter//Desktop//coursera//expl_data_an")
-data<-read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings="?")
+data<-read.table("household_power_consumption.txt", header=TRUE, sep=";", 
+                 na.strings="?")
 
 ##To date transformation
 data$Date<-as.Date(data$Date,format="%d/%m/%Y")
@@ -9,7 +10,8 @@ data$Date<-as.Date(data$Date,format="%d/%m/%Y")
 subset<-subset(data,Date=="2007-02-01" | Date=="2007-02-02")
 
 ## building plot
-hist(subset$Global_active_power,breaks=11,main="Global Active Power",xlab="Global Active Power (kilowatts)", col="red")
+hist(subset$Global_active_power,breaks=11,main="Global Active Power",
+     xlab="Global Active Power (kilowatts)", col="red")
 
 ##saving as png
 dev.copy(png,'plot1.png',width=480,height=480)
